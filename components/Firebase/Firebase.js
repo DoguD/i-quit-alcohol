1eba4d839f7268b010b3fbca07ffd8a2b4811a4a
DoguD <dogudeniz.ugur@gmail.com>
2023-10-28 01:00:00 +0800

    Firebase caused Next.js build issues fixed

--- components/Firebase/Firebase.js
+++ components/Firebase/Firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics, isSupported } from "firebase/analytics";
 import {getAuth, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,10 +18,17 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
+let app;
+// let analytics;
+let provider;
+let auth;
+
+if (firebaseConfig?.projectId) {
+    app = initializeApp(firebaseConfig);
+    auth = getAuth();
+    // analytics = getAnalytics(app);
+    provider = new GoogleAuthProvider();
+}
 
 export function signInWithGoogle() {
     signInWithRedirect(auth, provider);
